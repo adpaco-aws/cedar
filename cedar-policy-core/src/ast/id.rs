1d0e741716f5045bf3d8547b54ff23440d924d75
adpaco-aws <73246657+adpaco-aws@users.noreply.github.com>
2026-08-16 01:10:08 +0000

    Apply clippy fixes

--- cedar-policy-core/src/ast/id.rs
+++ cedar-policy-core/src/ast/id.rs
@@ -71,7 +71,7 @@ impl AsRef<str> for Id {
 
 impl std::fmt::Display for Id {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", &self.0)
+        write!(f, "{}", self.0)
     }
 }
 
@@ -330,7 +330,7 @@ impl AsRef<str> for AnyId {
 
 impl std::fmt::Display for AnyId {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", &self.0)
+        write!(f, "{}", self.0)
     }
 }
 
